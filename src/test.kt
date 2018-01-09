100ac7dc4710c63a62e09ddab163dd4543b3da33
alatel01 <alatel01@dcs.bbk.ac.uk>
2018-01-09 15:22:24 +0000

    Initial commit

---
+++ src/test.kt
@@ -0,0 +1,8 @@
+/**
+ * Created by alatel01 on 09/01/2018.
+ */
+fun main(args:Array<String>) {
+    print("Hello World! " + aFunc(-1))
+}
+
+fun aFunc(i:Int) = 0
---
+++ test/TestKtTest.kt
@@ -0,0 +1,14 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Created by alatel01 on 09/01/2018.
+ */
+class TestKtTest {
+    @Test
+    fun aFunc() {
+
+    }
+
+}
